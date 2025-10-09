31fc0a89ec7e331d9b071f8118b32aa6b80d10ab
Raksa <hoeunraksa012@gmail.com>
2025-10-09 21:06:23 +0700

    ....

---
+++ AssigementOOADWinForms/Controls/UserControlPayment.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace AssigementOOADWinForms.Controls
+{
+    partial class UserControlPayment
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            panel3 = new Panel();
+            label14 = new Label();
+            label13 = new Label();
+            pictureBox13 = new PictureBox();
+            flpProducts = new FlowLayoutPanel();
+            label5 = new Label();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            btnRefresh = new Button();
+            comboBox2 = new ComboBox();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Cursor = Cursors.Hand;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(31, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 28);
+            label1.TabIndex = 136;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 15F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(15, 42);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(224, 36);
+            comboBox1.TabIndex = 135;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.DodgerBlue;
+            panel3.Controls.Add(label14);
+            panel3.Controls.Add(label13);
+            panel3.Controls.Add(pictureBox13);
+            panel3.Location = new Point(644, 486);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(323, 115);
+            panel3.TabIndex = 134;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Cursor = Cursors.Hand;
+            label14.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label14.ForeColor = Color.White;
+            label14.Location = new Point(54, 57);
+            label14.Name = "label14";
+            label14.Size = new Size(97, 37);
+            label14.TabIndex = 28;
+            label14.Text = "12000";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.DodgerBlue;
+            label13.Cursor = Cursors.Hand;
+            label13.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label13.ForeColor = Color.White;
+            label13.Location = new Point(23, 18);
+            label13.Name = "label13";
+            label13.Size = new Size(190, 28);
+            label13.TabIndex = 27;
+            label13.Text = "Monthly Payments";
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.BackgroundImage = Properties.Resources.icons8_employees_100__1_;
+            pictureBox13.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox13.Cursor = Cursors.AppStarting;
+            pictureBox13.Location = new Point(273, 18);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(30, 30);
+            pictureBox13.TabIndex = 27;
+            pictureBox13.TabStop = false;
+            // 
+            // flpProducts
+            // 
+            flpProducts.BackColor = Color.White;
+            flpProducts.Location = new Point(15, 84);
+            flpProducts.Name = "flpProducts";
+            flpProducts.Size = new Size(623, 574);
+            flpProducts.TabIndex = 133;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Cursor = Cursors.Hand;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.ForeColor = Color.FromArgb(64, 64, 64);
+            label5.Location = new Point(644, 49);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 28);
+            label5.TabIndex = 130;
+            label5.Text = "SaleID";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.Font = new Font("Segoe UI", 15F);
+            label6.ForeColor = Color.FromArgb(64, 64, 64);
+            label6.Location = new Point(644, 262);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 28);
+            label6.TabIndex = 127;
+            label6.Text = "AmountPaid";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(644, 293);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(323, 37);
+            textBox5.TabIndex = 126;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DodgerBlue;
+            button2.FlatAppearance.BorderColor = Color.DodgerBlue;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Location = new Point(644, 621);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 37);
+            button2.TabIndex = 125;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.FlatAppearance.BorderColor = Color.DodgerBlue;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Location = new Point(838, 621);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 37);
+            button1.TabIndex = 124;
+            button1.Text = "Edit";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Cursor = Cursors.Hand;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.ForeColor = Color.FromArgb(64, 64, 64);
+            label4.Location = new Point(644, 191);
+            label4.Name = "label4";
+            label4.Size = new Size(157, 28);
+            label4.TabIndex = 123;
+            label4.Text = "PaymentMethod";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(644, 222);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(323, 37);
+            textBox4.TabIndex = 122;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(644, 151);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(323, 37);
+            textBox3.TabIndex = 121;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.ForeColor = Color.FromArgb(64, 64, 64);
+            label3.Location = new Point(644, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 28);
+            label3.TabIndex = 120;
+            label3.Text = "PaymentDate";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(644, 80);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(323, 37);
+            textBox2.TabIndex = 119;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.BackColor = Color.DodgerBlue;
+            btnRefresh.FlatAppearance.BorderColor = Color.DodgerBlue;
+            btnRefresh.FlatAppearance.BorderSize = 0;
+            btnRefresh.Location = new Point(500, 41);
+            btnRefresh.Margin = new Padding(0);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(129, 37);
+            btnRefresh.TabIndex = 118;
+            btnRefresh.Text = "Add User";
+            btnRefresh.UseVisualStyleBackColor = false;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Segoe UI", 15F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(258, 41);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(224, 36);
+            comboBox2.TabIndex = 137;
+            // 
+            // UserControlPayment
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(comboBox2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(panel3);
+            Controls.Add(flpProducts);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(btnRefresh);
+            Name = "UserControlPayment";
+            Size = new Size(984, 668);
+            Load += UserControlPayment_Load;
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Panel panel3;
+        private Label label14;
+        private Label label13;
+        private PictureBox pictureBox13;
+        private FlowLayoutPanel flpProducts;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox5;
+        private Button button2;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Button btnRefresh;
+        private ComboBox comboBox2;
+    }
+}
---
+++ InventoryAPI/Enum/UserRole.cs
@@ -0,0 +1,15 @@
+// File path: InventoryAPI/Enum/UserRole.cs
+
+namespace InventoryAPI.Enum
+{
+    /// <summary>
+    /// Defines roles for the users in the system.
+    /// Can be extended in the future (e.g., Supervisor, Guest, etc.)
+    /// </summary>
+    public enum UserRole
+    {
+        Admin,
+        Manager,
+        Staff
+    }
+}
