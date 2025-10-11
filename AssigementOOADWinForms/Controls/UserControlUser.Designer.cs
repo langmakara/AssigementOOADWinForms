c2cf312f8dc650fe06da957679b7bc2319623393
langmakara <langmakara122@gmail.com>
2025-10-11 15:10:25 +0700

    ...

--- AssigementOOADWinForms/Controls/UserControlUser.Designer.cs
+++ AssigementOOADWinForms/Controls/UserControlUser.Designer.cs
@@ -48,9 +48,11 @@
             btInsert = new Button();
             panel1 = new Panel();
             cbActive = new CheckBox();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)dgvuser).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbProfile).BeginInit();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // dgvuser
@@ -58,11 +60,11 @@
             dgvuser.BackgroundColor = Color.White;
             dgvuser.BorderStyle = BorderStyle.Fixed3D;
             dgvuser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvuser.Location = new Point(8, 103);
+            dgvuser.Location = new Point(3, 115);
             dgvuser.Margin = new Padding(3, 4, 3, 4);
             dgvuser.Name = "dgvuser";
             dgvuser.RowHeadersWidth = 51;
-            dgvuser.Size = new Size(771, 760);
+            dgvuser.Size = new Size(819, 766);
             dgvuser.TabIndex = 2;
             // 
             // label2
@@ -70,8 +72,8 @@
             label2.AutoSize = true;
             label2.Cursor = Cursors.Hand;
             label2.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.FromArgb(64, 64, 64);
-            label2.Location = new Point(302, 8);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(297, 5);
             label2.Name = "label2";
             label2.Size = new Size(65, 35);
             label2.TabIndex = 37;
@@ -79,12 +81,12 @@
             // 
             // btClear
             // 
-            btClear.BackColor = Color.DodgerBlue;
+            btClear.BackColor = Color.Orange;
             btClear.FlatAppearance.BorderColor = Color.DodgerBlue;
             btClear.FlatAppearance.BorderSize = 0;
             btClear.Font = new Font("Bahnschrift", 13.8F, FontStyle.Bold);
             btClear.ForeColor = Color.White;
-            btClear.Location = new Point(634, 49);
+            btClear.Location = new Point(629, 46);
             btClear.Margin = new Padding(0);
             btClear.Name = "btClear";
             btClear.Size = new Size(147, 49);
@@ -97,8 +99,8 @@
             lbSeach.AutoSize = true;
             lbSeach.Cursor = Cursors.Hand;
             lbSeach.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbSeach.ForeColor = Color.FromArgb(64, 64, 64);
-            lbSeach.Location = new Point(8, 7);
+            lbSeach.ForeColor = Color.White;
+            lbSeach.Location = new Point(10, 4);
             lbSeach.Name = "lbSeach";
             lbSeach.Size = new Size(82, 35);
             lbSeach.TabIndex = 35;
@@ -106,7 +108,7 @@
             // 
             // tbSeach
             // 
-            tbSeach.Location = new Point(8, 48);
+            tbSeach.Location = new Point(10, 45);
             tbSeach.Margin = new Padding(3, 4, 3, 4);
             tbSeach.Multiline = true;
             tbSeach.Name = "tbSeach";
@@ -117,7 +119,7 @@
             // 
             cbRole1.Font = new Font("Segoe UI", 15F);
             cbRole1.FormattingEnabled = true;
-            cbRole1.Location = new Point(297, 49);
+            cbRole1.Location = new Point(292, 46);
             cbRole1.Margin = new Padding(3, 4, 3, 4);
             cbRole1.Name = "cbRole1";
             cbRole1.Size = new Size(285, 43);
@@ -141,7 +143,7 @@
             lbUsername.AutoSize = true;
             lbUsername.Cursor = Cursors.Hand;
             lbUsername.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbUsername.ForeColor = Color.FromArgb(64, 64, 64);
+            lbUsername.ForeColor = Color.White;
             lbUsername.Location = new Point(49, 425);
             lbUsername.Name = "lbUsername";
             lbUsername.Size = new Size(129, 35);
@@ -171,7 +173,7 @@
             lbPassword.AutoSize = true;
             lbPassword.Cursor = Cursors.Hand;
             lbPassword.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbPassword.ForeColor = Color.FromArgb(64, 64, 64);
+            lbPassword.ForeColor = Color.White;
             lbPassword.Location = new Point(49, 526);
             lbPassword.Name = "lbPassword";
             lbPassword.Size = new Size(122, 35);
@@ -183,7 +185,7 @@
             lbID.AutoSize = true;
             lbID.Cursor = Cursors.Hand;
             lbID.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbID.ForeColor = Color.FromArgb(64, 64, 64);
+            lbID.ForeColor = Color.White;
             lbID.Location = new Point(49, 319);
             lbID.Name = "lbID";
             lbID.Size = new Size(40, 35);
@@ -234,7 +236,7 @@
             lbRole.AutoSize = true;
             lbRole.Cursor = Cursors.Hand;
             lbRole.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbRole.ForeColor = Color.FromArgb(64, 64, 64);
+            lbRole.ForeColor = Color.White;
             lbRole.Location = new Point(49, 627);
             lbRole.Name = "lbRole";
             lbRole.Size = new Size(65, 35);
@@ -253,7 +255,7 @@
             // 
             // btInsert
             // 
-            btInsert.BackColor = Color.DodgerBlue;
+            btInsert.BackColor = Color.Orange;
             btInsert.FlatAppearance.BorderColor = Color.DodgerBlue;
             btInsert.FlatAppearance.BorderSize = 0;
             btInsert.Font = new Font("Bahnschrift", 13.8F, FontStyle.Bold);
@@ -268,7 +270,7 @@
             // 
             // panel1
             // 
-            panel1.BackColor = Color.White;
+            panel1.BackColor = Color.DodgerBlue;
             panel1.BorderStyle = BorderStyle.Fixed3D;
             panel1.Controls.Add(cbActive);
             panel1.Controls.Add(btInsert);
@@ -283,15 +285,16 @@
             panel1.Controls.Add(lbUsername);
             panel1.Controls.Add(tbID);
             panel1.Controls.Add(pbProfile);
-            panel1.Location = new Point(806, 19);
+            panel1.Location = new Point(828, -2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(438, 844);
+            panel1.Size = new Size(438, 894);
             panel1.TabIndex = 50;
             // 
             // cbActive
             // 
             cbActive.AutoSize = true;
             cbActive.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            cbActive.ForeColor = Color.White;
             cbActive.Location = new Point(286, 319);
             cbActive.Name = "cbActive";
             cbActive.Size = new Size(88, 29);
@@ -299,17 +302,26 @@
             cbActive.Text = "Active";
             cbActive.UseVisualStyleBackColor = true;
             // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DodgerBlue;
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(btClear);
+            panel2.Controls.Add(lbSeach);
+            panel2.Controls.Add(tbSeach);
+            panel2.Controls.Add(cbRole1);
+            panel2.Location = new Point(-5, -2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(835, 114);
+            panel2.TabIndex = 51;
+            // 
             // UserControlUser
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Azure;
+            Controls.Add(panel2);
             Controls.Add(panel1);
-            Controls.Add(label2);
-            Controls.Add(btClear);
-            Controls.Add(lbSeach);
-            Controls.Add(tbSeach);
-            Controls.Add(cbRole1);
             Controls.Add(dgvuser);
             Margin = new Padding(3, 4, 3, 4);
             Name = "UserControlUser";
@@ -318,8 +330,9 @@
             ((System.ComponentModel.ISupportInitialize)pbProfile).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -344,5 +357,6 @@
         private Button btInsert;
         private Panel panel1;
         private CheckBox cbActive;
+        private Panel panel2;
     }
 }
